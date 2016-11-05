1db2c13ce7b00397a681c677611e536b3d766591
Meelik Kiik <kiik.meelik@gmail.com>
2016-11-05 19:55:07 +0200

    Update environment passage.

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,10 +1,9 @@
 
+var env = require('..');
+
 module.exports = {
   'create_app': function() {
     var factory = require('./...');
-    return factory.create_app();
-  },
-
-  // Dependencies
-  'env': require('..'),
+    return factory.create_app(env);
+  }
 }
